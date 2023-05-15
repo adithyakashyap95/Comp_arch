3fd304ac65850af3e24c7c13b8c0aa465d9d78c9
Adithya Rajagopal <adithyakashyap95@gmail.com>
2023-05-15 18:49:29 -0500

    Initial Commit

---
+++ Comp_arch/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Comp_arch
+//
+//  Created by Adithya Kashyap Rajagopal on 5/15/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

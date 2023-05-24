c565bef6d832a649559298b0c200cc6934b762cd
adithyakashyap95 <adit5@auto.ece.pdx.edu>
2023-05-24 11:20:57 -0700

    added all files

--- systemverilog/scripts/run_tb_fe.sh
+++ systemverilog/scripts/run_tb_fe.sh
@@ -3,5 +3,12 @@
 /pkgs/mentor/questa/10.6b/questasim/bin/vmap work
 /pkgs/mentor/questa/10.6b/questasim/bin/vlog ../rtl/main.sv
 /pkgs/mentor/questa/10.6b/questasim/bin/vlog ../rtl/tb.sv
+/pkgs/mentor/questa/10.6b/questasim/bin/vlog ../rtl/df.sv
+/pkgs/mentor/questa/10.6b/questasim/bin/vlog ../rtl/dh.sv
+/pkgs/mentor/questa/10.6b/questasim/bin/vlog ../rtl/ex.sv
+/pkgs/mentor/questa/10.6b/questasim/bin/vlog ../rtl/id.sv
+/pkgs/mentor/questa/10.6b/questasim/bin/vlog ../rtl/if.sv
+/pkgs/mentor/questa/10.6b/questasim/bin/vlog ../rtl/mem.sv
+/pkgs/mentor/questa/10.6b/questasim/bin/vlog ../rtl/wb.sv
 /pkgs/mentor/questa/10.6b/questasim/bin/vsim -debugDB tb -do  "add wave sim:/tb/* ; run -all"
 

1d0c5ffa3a711d68858e5a3537b325a59f7f284c
preetambasti <pbasti@pdx.edu>
2023-06-03 15:53:31 -0700

    hazard included

--- systemverilog/rtl/dh.sv
+++ systemverilog/rtl/dh.sv
@@ -1,3 +1,20 @@
 // data hazard
 
 `include "../rtl/struct.sv"
+
+module hazzo(
+
+input logic ifid_memread,
+input logic idex_dest,
+input logic [4:0] ifid_rs,
+input logic [4:0] ifid_rt,
+
+output logic hazard
+);
+
+
+
+assign hazard = ifid_memread & (idex_dest != 0) & ((idex_dest == ifid_rs) | (idex_dest == ifid_rt));
+
+endmodule
+
